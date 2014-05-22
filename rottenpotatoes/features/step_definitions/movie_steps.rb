0247926b277fdc6d5ee89a139326b48a09ddf4c5
Sam Joseph <tansaku@gmail.com>
2014-05-22 12:11:00 -0300

    adds flunks to other unimplemented steps

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -25,8 +25,10 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  flunk "Unimplemented"
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
+  flunk "Unimplemented"
 end
