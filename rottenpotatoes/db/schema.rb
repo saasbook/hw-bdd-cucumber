7d27588fe9e387898d041b54bc3a5a388b70a510
saasbook <saasbook@saasbook.(none)>
2015-07-07 16:55:48 -0300

    skeletonDone

--- rottenpotatoes/db/schema.rb
+++ rottenpotatoes/db/schema.rb
@@ -9,13 +9,13 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120130161449) do
+ActiveRecord::Schema.define(version: 20120130161449) do
 
-  create_table "movies", :force => true do |t|
-    t.string   "title"
-    t.string   "rating"
+  create_table "movies", force: :cascade do |t|
+    t.string   "title",        limit: 255
+    t.string   "rating",       limit: 255
     t.text     "description"
     t.datetime "release_date"
     t.datetime "created_at"
