1e03e4496c7c361cf3099e5e699438d7ae6a33a7
Sam Joseph <tansaku@gmail.com>
2017-10-02 14:59:56 +0100

    Update schema.rb

--- rottenpotatoes/db/schema.rb
+++ rottenpotatoes/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20120130161449) do
+ActiveRecord::Schema.define(version: 20111119180638) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title",        limit: 255
