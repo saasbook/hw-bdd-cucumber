4e78d64e7a87d1b1e193eae5d215bbd02916b5b6
ThaweesukKMUTNB <s6301012630044@email.kmutnb.ac.th>
2022-10-28 23:54:55 +0700

    part-0

--- rottenpotatoes/features/support/paths.rb
+++ rottenpotatoes/features/support/paths.rb
@@ -13,17 +13,24 @@ module NavigationHelpers
   def path_to(page_name)
     case page_name
 
-    when /^the movies page$/ then '/movies'
-      
-      # Here is an example that uses value from the Regexp:
-      #
-      #   when /^the details page for movie "(.*)"$/ do |movie_name|
-      #     movie_path(Movie.find_by(:name => movie_name))
-      # end
+    when /^the home\s?page$/
+      '/'
+
+    # Add more mappings here.
+    # Here is an example that pulls values out of the Regexp:
+    #
+    #   when /^(.*)'s profile page$/i
+    #     user_profile_path(User.find_by_login($1))
 
     else
-      raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
-        "Now, go and add a mapping in #{__FILE__}"
+      begin
+        page_name =~ /^the (.*) page$/
+        path_components = $1.split(/\s+/)
+        self.send(path_components.push('path').join('_').to_sym)
+      rescue NoMethodError, ArgumentError
+        raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
+          "Now, go and add a mapping in #{__FILE__}"
+      end
     end
   end
 end
